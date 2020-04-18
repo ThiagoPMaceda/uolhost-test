2f47789a3c016c8cff85ba7cdf80d088e8940aa1
Thiago P <thiagopmaceda@gmail.com>
2020-04-18 20:52:20 -0300

    Adicionando o modulo de customer-panel.

---
+++ src/app/customer-panel/customer-panel.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerPanelComponent } from './customer-panel.component';
+
+
+
+@NgModule({
+  declarations: [CustomerPanelComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CustomerPanelModule { }
