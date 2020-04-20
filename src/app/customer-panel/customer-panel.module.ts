6b2049f9ec3afd30387ed8bdf78fb4a4f362447d
Thiago P <thiagopmaceda@gmail.com>
2020-04-20 13:30:12 -0300

    Adicionado componente customer-list nas declarations.

--- src/app/customer-panel/customer-panel.module.ts
+++ src/app/customer-panel/customer-panel.module.ts
@@ -1,13 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CustomerPanelComponent } from './customer-panel.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CustomerPanelComponent } from "./customer-panel.component";
+import { CustomerListComponent } from "./customer-list/customer-list.component";
 
 @NgModule({
-  declarations: [CustomerPanelComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CustomerPanelComponent, CustomerListComponent],
+  imports: [CommonModule],
 })
-export class CustomerPanelModule { }
+export class CustomerPanelModule {}
