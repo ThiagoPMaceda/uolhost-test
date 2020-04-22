015e6ccfbf7bdba7b72ec902b12fd528df6237d4
Thiago P <thiagopmaceda@gmail.com>
2020-04-22 10:56:09 -0300

    Adicionado property binding para receber os dados dinamicamente do customer-panel

--- src/app/customer-panel/customer-list/customer-list.component.ts
+++ src/app/customer-panel/customer-list/customer-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-customer-list',
-  templateUrl: './customer-list.component.html',
-  styleUrls: ['./customer-list.component.scss']
+  selector: "app-customer-list",
+  templateUrl: "./customer-list.component.html",
+  styleUrls: ["./customer-list.component.scss"],
 })
 export class CustomerListComponent implements OnInit {
+  @Input() customerName: string;
+  @Input() email: string;
+  @Input() cpf: string;
+  @Input() number: string;
+  @Input() status: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
