6d94935f63e30b15f8c65a5cc1ce24d4f26c3f42
Thiago P <thiagopmaceda@gmail.com>
2020-04-22 10:58:49 -0300

    Implementando a service de clientes.

--- src/app/customer-panel/customer-panel.component.ts
+++ src/app/customer-panel/customer-panel.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { CustomerService } from "./customer.service";
+import { Component, OnInit } from "@angular/core";
+import { Customer } from "./customer";
 
 @Component({
-  selector: 'app-customer-panel',
-  templateUrl: './customer-panel.component.html',
-  styleUrls: ['./customer-panel.component.scss']
+  selector: "app-customer-panel",
+  templateUrl: "./customer-panel.component.html",
+  styleUrls: ["./customer-panel.component.scss"],
 })
 export class CustomerPanelComponent implements OnInit {
+  customersArray;
 
-  constructor() { }
+  constructor(private customerService: CustomerService) {}
 
   ngOnInit(): void {
+    this.customerService.getCustomers().subscribe((customer: Customer[]) => {
+      this.customersArray = customer;
+    });
   }
-
 }
